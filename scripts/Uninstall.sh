29795b3ed4d963d65164ba0cf17c8a704bbb45a9
perfect <skyqqcc@gmail.com>
2019-09-19 21:33:15 +0800

    Revert "Initial commit"
    This reverts commit de5a686621d936e138a0fba6997b224cca9f0c61.

--- scripts/Uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-systemctl stop shadowsocks-multiuser
-rm -f /etc/systemd/system/shadowsocks-multiuser.service
-
-rm -rf /opt/shadowsocks-multiuser
-
-userdel -r -f shadowsocks-multiuser
-
-exit 0
